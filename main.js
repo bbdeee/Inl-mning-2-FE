e3ddcc81d46611e27c672088dd824261e5893c8e
Brian David <brian.david1881@gmail.com>
2024-01-31 12:44:40 +0100

    1244

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 async function start(){
-    let response = await fetch ('https://pixabay.com/api/');
+    let response = await fetch ('https://pixabay.com/api?key=42129869-dc478a785dd85fbec059c7ef2&q=yellow+flower&image_type=photo');
+    let json= await response.json();
+
+    let tag = json.total;
+    let p = document.querySelector('p');
+    p.textContent = tag;
+
+    let imgElement = document.createElement('img');
+    imgElement.src = ''
+    
+
 }
+
+start();
