8b414f3b59e34d6240fc37e72f9d924c63c162d5
JohnLjungblad <ljungblad.john@gmail.com>
2024-02-01 12:10:25 +0100

    1210

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
-let searchButton = document.querySelector('.search-button');
+let main = document.querySelector('#main');
+let footer = document.querySelector('#footer');
+let searchForm = document.querySelector('#search-form');
 
-async function start(){
-    let response = await fetch ('https://pixabay.com/api?key=42129869-dc478a785dd85fbec059c7ef2&q=yellow+flower&image_type=photo');
+async function search(){
+    let searchPhrase = document.querySelector('#search-bar').value;
+    let searchColor = document.querySelector('#color-selector').value;
+
+    let response = await fetch ('https://pixabay.com/api?key=42129869-dc478a785dd85fbec059c7ef2&q='+ searchColor +'+' + searchPhrase +'&image_type=photo');
     let json= await response.json();
 
-    let tag = json.total;
-    let p = document.querySelector('p');
-    p.textContent = tag;
-
-    let firstimg= json.hits[0];
-
-    let imgElement = document.createElement('img');
-    imgElement.src = firstimg.previewURL;
     
-    document.body.append(imgElement);
 
+    for(let hit in json.hits){
+        let img = json.hits[hit].previewURL;
+        let tag = json.hits[hit].tags;
+        let user = json.hits[hit].user;
+
+        let imgElement = document.createElement('img');
+        let tagParagraph = document.createElement('p');
+        let userParagraph = document.createElement('p');
+
+        imgElement.src = img;
+        tagParagraph.textContent = tag;
+        userParagraph.textContent = user;
+
+        main.appendChild(tagParagraph);
+        main.appendChild(userParagraph);
+        main.appendChild(imgElement);
+
+        /*let previousButton = document.createElement('button');
+        let nextButton = document.createElement('button');
+
+        footer.appendChild(previousButton);
+        footer.appendChild(nextButton);*/
+    }
+
+    
 }
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    search(); //Load image 
 
-
-searchButton.onclick = function () {
-    start();
-};
+});
