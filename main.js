75a9bc8857cd38eff80770c1169da96d4456fade
JohnLjungblad <ljungblad.john@gmail.com>
2024-02-05 12:27:44 +0100

    1227

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 //Reaching elements with variables
-let main = document.querySelector('#main');
-let footer = document.querySelector('#footer');
-let searchForm = document.querySelector('#search-form');
+const main = document.querySelector('#main');
+const footer = document.querySelector('#footer');
+const searchForm = document.querySelector('#search-form');
 //Keys
-let johnKey = '42129869-dc478a785dd85fbec059c7ef2';
-let brianKey = '42149569-386fe2868d6733db199f6e4c1';
+const johnKey = '42129869-dc478a785dd85fbec059c7ef2';
+const brianKey = '42149569-386fe2868d6733db199f6e4c1';
 //Create the invisble buttons
 let previousButton = document.createElement('button');
 let nextButton = document.createElement('button');
+// Picture load "counter"
+let start = 0; 
+let stop = 9; 
+//create json to be able assign it in search() and use it in load()
+let json = null;
 
 async function search(){
     //Reaching elements with variables
@@ -17,11 +22,17 @@ async function search(){
     let response = await fetch ('https://pixabay.com/api?key='+brianKey+'&q='+ searchColor +'+' + searchPhrase +'&image_type=photo');
     let json= await response.json();
 
-    for(let hit in json.hits){
+    return json;
+}
+
+async function load(json){
+
+    for(let i = start; i <= stop; i++){
+        
         //Find data from array
-        let img = json.hits[hit].previewURL;
-        let tag = json.hits[hit].tags;
-        let user = json.hits[hit].user;
+        let img = json.hits[i].previewURL;
+        let tag = json.hits[i].tags;
+        let user = json.hits[i].user;
         //Create (invisible) elements
         let imgElement = document.createElement('img');
         let tagParagraph = document.createElement('p');
@@ -29,7 +40,7 @@ async function search(){
         let contentContainer = document.createElement('div');
 
         //Set attribue for div container
-        contentContainer.setAttribute('class', 'content-container');
+        contentContainer.setAttribute('id', 'content-container');
 
         //Set data for the invisible elements recently created
         imgElement.src = img;
@@ -42,6 +53,7 @@ async function search(){
         contentContainer.appendChild(imgElement);
         main.appendChild(contentContainer);
     }
+
     //Text for buttons
     previousButton.textContent = 'Previous page';
     nextButton.textContent = 'Next page';
@@ -49,15 +61,45 @@ async function search(){
     footer.appendChild(previousButton);
     footer.appendChild(nextButton); 
 }
-searchForm.addEventListener('submit', function (event) {
+
+async function remove(){
+    for(let i = 0; i < 10; i++){
+        let contentContainer = document.querySelector('#content-container'); 
+
+        if(contentContainer){
+            contentContainer.remove();
+        }
+    }  
+}
+
+searchForm.addEventListener('submit', async function (event) {
     event.preventDefault();
-    search(); //Load image 
+    start = 0;
+    stop = 9;
+    remove();
+    json = await search(); //Get Json
+    load(json); // Load data
 
 });
 //Functions for buttons
 nextButton.onclick = function(){
-    console.log("Next");
+    if(stop < json.hits.length -1){
+        console.log("Next");
+        start = start + 10; 
+        stop = stop + 10; 
+        remove();
+        load(json);
+    }
 };
 previousButton.onclick = function(){
     console.log("Previous");
+    if(start > 0){
+        start = start - 10; 
+        stop = stop - 10; 
+        remove();
+        load(json);
+    }
 };
+
+//1. Any color!!!!
+//2. Css
