cafef8fe5d73debbc09499b394cfc2e70263e439
JohnLjungblad <ljungblad.john@gmail.com>
2024-02-08 12:27:03 +0100

    1227

--- main.js
+++ main.js
@@ -16,6 +16,16 @@ contentContainer.setAttribute('id', 'content-container');
 //Create div for buttons
 let buttonDiv = document.createElement('div');
 buttonDiv.setAttribute('id', 'buttons');
+//Designed buttons (not rly button)
+let nextNotButton = document.createElement('a');
+let nextNotButtonSpan = document.createElement('span');
+let previousNotButton = document.createElement('a');
+let previousNotButtonSpan = document.createElement('span');
+//Attributes for notbuttons
+nextNotButton.setAttribute('class', 'design-buttons');
+previousNotButton.setAttribute('class', 'design-buttons');
+nextNotButtonSpan.setAttribute('class', 'design-buttons-span');
+previousNotButtonSpan.setAttribute('class', 'design-buttons-span');
 // Picture load "counter"
 let start = 0; 
 let stop = 9; 
@@ -23,11 +33,15 @@ let stop = 9;
 let json = null;
 
 async function search(){
-    //Reaching elements with variables
+        //Reaching elements with variables
     let searchPhrase = document.querySelector('#search-bar').value;
     let searchColor = document.querySelector('#color-selector').value;
+
+    if(searchColor == 'any color'){
+        searchColor = 'red|black|orange|yellow|white|brown|blue|pink|purple|green|grey|turquoise';
+    }
     //Api things
-    let response = await fetch ('https://pixabay.com/api?key='+brianKey+'&q='+ searchColor +'+' + searchPhrase +'&image_type=photo');
+    let response = await fetch ('https://pixabay.com/api?key='+brianKey+'&q='+ searchColor +'+' + searchPhrase +'&image_type=photo&per_page=50');
     let json= await response.json();
 
     //Load content-container div
@@ -74,12 +88,21 @@ async function load(json){
     }
     //Append div
     footer.appendChild(buttonDiv);
-    //Text for buttons
-    previousButton.textContent = 'Previous page';
-    nextButton.textContent = 'Next page';
+    //Text for buttons 
+    //previousButton.textContent = 'Previous page';
+    //nextButton.textContent = 'Next page';
     //Show buttons
-    buttonDiv.appendChild(previousButton);
-    buttonDiv.appendChild(nextButton); 
+    //buttonDiv.appendChild(previousButton);
+    //buttonDiv.appendChild(nextButton); 
+
+    //New design "buttons"
+    buttonDiv.appendChild(previousNotButton);
+    buttonDiv.appendChild(nextNotButton);
+    nextNotButton.appendChild(nextNotButtonSpan);
+    previousNotButton.appendChild(previousNotButtonSpan);
+
+    nextNotButtonSpan.textContent = 'Next';
+    previousNotButtonSpan.textContent = 'Prev';
 }
 
 async function remove(){
@@ -99,27 +122,45 @@ searchForm.addEventListener('submit', async function (event) {
     remove();
     json = await search(); //Get Json
     load(json); // Load data
+    checkButtons();
+    
+    let numberPics = json.hits.length;
+console.log(numberPics);
 
 });
 //Functions for buttons
-nextButton.onclick = function(){
+nextNotButton.onclick = function(){
     if(stop < json.hits.length -1){
         console.log("Next");
         start = start + 10; 
         stop = stop + 10; 
         remove();
         load(json);
+        checkButtons();
     }
 };
-previousButton.onclick = function(){
+previousNotButton.onclick = function(){
     console.log("Previous");
     if(start > 0){
         start = start - 10; 
         stop = stop - 10; 
         remove();
         load(json);
+        checkButtons();
     }
 };
 
-//1. Any color!!!!
-//2. Css
+async function checkButtons(){
+    if(start > 0){
+        previousNotButton.classList.remove('disabled');
+        
+    }else{
+        previousNotButton.classList.add('disabled');
+    }
+    
+    if(stop < json.hits.length -1){
+        nextNotButton.classList.remove('disabled');
+    }else{
+        nextNotButton.classList.add('disabled');
+    }
+}
